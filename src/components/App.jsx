be935a79dbc3b9af676135296a3f0dbeaf17b4c9
Andrii Chychkan <mywork1728@gmail>
2023-08-23 22:29:17 +0300

    started adding useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
-import { Component } from 'react';
+import { Component, useState } from 'react';
 import { Section } from './Section/Section';
 import { Layout } from './Layout';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
 
-export class App extends Component {
+export const App = () => {
   state = {
     good: 0,
     neutral: 0,
     bad: 0,
   };
+
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState();
+  const [bad, setBad] = useState();
 
   onLeaveFeedback = option => {
     this.setState(prevState => {
@@ -30,46 +34,44 @@ export class App extends Component {
     return Math.round((this.state.good / total) * 100);
   };
 
-  render() {
-    const totalFeedback = this.countTotalFeedback();
-    const positivePercentage = this.countPositiveFeedbackPercentage();
+  const totalFeedback = this.countTotalFeedback();
+  const positivePercentage = this.countPositiveFeedbackPercentage();
 
-    if (totalFeedback === 0) {
-      return (
-        <Layout>
-          <Section title="Please Leave feedback">
-            <h1>{this.title}</h1>
-            <FeedbackOptions
-              onLeaveFeedback={this.onLeaveFeedback}
-              options={Object.keys(this.state)}
-            />
-          </Section>
-          <Section>
-            <Notification message="There is no feedback" />
-          </Section>
-        </Layout>
-      );
-    }
+  if (totalFeedback === 0) {
     return (
       <Layout>
         <Section title="Please Leave feedback">
-          <h1>{this.title}</h1>
+          <h1>{title}</h1>
           <FeedbackOptions
             onLeaveFeedback={this.onLeaveFeedback}
             options={Object.keys(this.state)}
           />
         </Section>
-        <Section title="Statistics">
-          <h1>{this.title}</h1>
-          <Statistics
-            goodGrade={this.state.good}
-            neutralGrade={this.state.neutral}
-            badGrade={this.state.bad}
-            sum={totalFeedback}
-            positivePercentage={positivePercentage}
-          />
+        <Section>
+          <Notification message="There is no feedback" />
         </Section>
       </Layout>
     );
   }
-}
+  return (
+    <Layout>
+      <Section title="Please Leave feedback">
+        <h1>{this.title}</h1>
+        <FeedbackOptions
+          onLeaveFeedback={onLeaveFeedback}
+          options={Object.keys(this.state)}
+        />
+      </Section>
+      <Section title="Statistics">
+        <h1>{this.title}</h1>
+        <Statistics
+          goodGrade={this.state.good}
+          neutralGrade={this.state.neutral}
+          badGrade={this.state.bad}
+          sum={totalFeedback}
+          positivePercentage={positivePercentage}
+        />
+      </Section>
+    </Layout>
+  );
+};
